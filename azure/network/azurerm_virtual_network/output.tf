fe0a4e2c5f5787d18a55ceb8d4446f60488f75b5
Anurag Vijay <anurag_vijay@Anurags-MacBook-Air.local>
2026-01-14 21:11:58 +0530

    output.tf modified for virtual_network module

--- azure/network/azurerm_virtual_network/output.tf
+++ azure/network/azurerm_virtual_network/output.tf
@@ -3,14 +3,11 @@ output "virtual_network_ids" {
     name          = azurerm_virtual_network.virtual_network.name
     address_space = azurerm_virtual_network.virtual_network.address_space
     id            = azurerm_virtual_network.virtual_network.id
-  }
-}
 
-output "subnet_ids" {
-  value = {
-    for subnet in azurerm_virtual_network.virtual_network.subnet : subnet.name => {
-      id               = subnet.id
-      address_prefixes = subnet.address_prefixes
-    }
-  }
+    subnets = {
+      for subnet in azurerm_virtual_network.virtual_network.subnet :
+      subnet.name => {
+        id               = subnet.id
+        address_prefixes = subnet.address_prefixes
+  } } }
 }
